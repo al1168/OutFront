8332996ea8d2ab7526e8b04a31a39dae43ebd8e4
DESKTOP-V6C3FIE\Alden <business.aldenlu@gmail.com>
2023-12-12 20:45:51 -0500

    resolve type error from google auth code

--- app/ui/login/LoginButton.tsx
+++ app/ui/login/LoginButton.tsx
@@ -1,44 +1,46 @@
-export default function LoginButton(){
-    /*
- * Create form to request access token from Google's OAuth 2.0 server.
- */
-function oauthSignIn() {
-    // Google's OAuth 2.0 endpoint for requesting an access token
-    var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+export default function LoginButton() {
+    function oauthSignIn() {
+        const oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
   
-    // Create <form> element to submit parameters to OAuth 2.0 endpoint.
-    var form = document.createElement('form');
-    form.setAttribute('method', 'GET'); // Send as a GET request.
-    form.setAttribute('action', oauth2Endpoint);
+        // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+        const form = document.createElement('form');
+        form.setAttribute('method', 'GET');
+        form.setAttribute('action', oauth2Endpoint);
   
-    // Parameters to pass to OAuth 2.0 endpoint.
-    var params = {'client_id': 'YOUR_CLIENT_ID',
-                  'redirect_uri': 'YOUR_REDIRECT_URI',
-                  'response_type': 'token',
-                  'scope': 'https://www.googleapis.com/auth/youtube.force-ssl',
-                  'include_granted_scopes': 'true',
-                  'state': 'pass-through value'};
+        // Parameters to pass to OAuth 2.0 endpoint.
+        const params: { [key: string]: string } = {
+            'client_id': 'YOUR_CLIENT_ID',
+            'redirect_uri': 'YOUR_REDIRECT_URI',
+            'response_type': 'token',
+            'scope': 'https://www.googleapis.com/auth/youtube.force-ssl',
+            'include_granted_scopes': 'true',
+            'state': 'pass-through value'
+        };
   
-    // Add form parameters as hidden input values.
-    for (var p in params) {
-      var input = document.createElement('input');
-      input.setAttribute('type', 'hidden');
-      input.setAttribute('name', p);
-      input.setAttribute('value', params[p]);
-      form.appendChild(input);
+        // Add form parameters as hidden input values.
+        for (const p in params) {
+            if (params.hasOwnProperty(p)) {
+                const input = document.createElement('input');
+                input.setAttribute('type', 'hidden');
+                input.setAttribute('name', p);
+                input.setAttribute('value', params[p]);
+                form.appendChild(input);
+            }
+        }
+  
+        // Add form to page and submit it to open the OAuth 2.0 endpoint.
+        document.body.appendChild(form);
+        form.submit();
     }
-  
-    // Add form to page and submit it to open the OAuth 2.0 endpoint.
-    document.body.appendChild(form);
-    form.submit();
-  }
+
     const handleOnClick = () => {
-        oauthSignIn()
-        console.log("wtf")
-    }
+        oauthSignIn();
+        console.log("wtf");
+    };
 
-    return  <button onClick={handleOnClick} className='border-2 border-gray-300 rounded-full w-16 h-16 p-2'>
-    login
-  </button>
-
+    return (
+        <button onClick={handleOnClick} className='border-2 border-gray-300 rounded-full w-16 h-16 p-2'>
+            login
+        </button>
+    );
 }
